f0074389e47fe54102713197ae42d87bc26d9cd3
Shaik077 <tshaikvali@gmail.com>
2020-07-06 15:56:54 +0530

    [Shaik]ADDED:Use RANDOM FUNCTION For SimulationForBet

--- Gambling-Simulation-Problem.sh
+++ Gambling-Simulation-Problem.sh
@@ -2,3 +2,17 @@
 
 STAKE_PER_DAY=100
 BET_PER_GAME=1
+simulateOneGame(){
+	WIN=1
+	LOSE=0
+
+	Check=$((RANDOM%2))
+	if [ $Check -eq $WIN ];
+	then
+		ResultAmount=$BET_PER_GAME
+	else
+		ResultAmount=-$BET_PER_GAME
+	fi
+}
+simulateOneGame
+echo $ResultAmount
